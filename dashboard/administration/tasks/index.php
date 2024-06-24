26a69db9ddd152a5a24943f03ab3d7ab3484ee88
Nick Derry <nickd@caliwebdesignservices.com>
2024-06-24 06:57:23 +0000

    Modified tasks page in admin center to check employee ranking.

--- dashboard/administration/tasks/index.php
+++ dashboard/administration/tasks/index.php
@@ -33,55 +33,159 @@
                     <div class="card-body">
                         <div class="dashboard-table">
                             <?php
-                                $sql = "SELECT * FROM caliweb_tasks";
-                                $result = mysqli_query($con, $sql);
+                                if ($employeeAccessLevel == "Executive") {
+                                    $sql = "SELECT * FROM caliweb_tasks";
+                                    $result = mysqli_query($con, $sql);
 
-                                if (mysqli_num_rows($result) > 0) {
-                                    // Output table header
-                                    echo '<table style="width:100%; margin-top:-3%;">
-                                            <tr>
-                                                <th style="width:20%;">Task Name</th>
-                                                <th style="width:20%;">Task Start Date</th>
-                                                <th style="width:20%;">Task Due Date</th>
-                                                <th style="width:20%;">Status</th>
-                                                <th style="width:20%;">Actions</th>
-                                            </tr>';
+                                    if (mysqli_num_rows($result) > 0) {
+                                        // Output table header
+                                        echo '<table style="width:100%; margin-top:-3%;">
+                                                <tr>
+                                                    <th style="width:20%;">Task Name</th>
+                                                    <th style="width:20%;">Task Start Date</th>
+                                                    <th style="width:20%;">Task Due Date</th>
+                                                    <th style="width:20%;">Status</th>
+                                                    <th style="width:20%;">Actions</th>
+                                                </tr>';
 
-                                    // Output table rows
-                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        // Output table rows
+                                        while ($row = mysqli_fetch_assoc($result)) {
 
-                                        $tasksStatusColorAssignment = $row['status'];
+                                            $tasksStatusColorAssignment = $row['status'];
 
-                                        $startDate = $row['taskStartDate'];
-                                        $startDateFormatted = new DateTime($startDate);
-                                        $startDateFormattedFinal = $startDateFormatted->format('F j, Y g:i A');
+                                            $startDate = $row['taskStartDate'];
+                                            $startDateFormatted = new DateTime($startDate);
+                                            $startDateFormattedFinal = $startDateFormatted->format('F j, Y g:i A');
 
-                                        $dueDate = $row['taskDueDate'];
-                                        $dueDateFormatted = new DateTime($dueDate);
-                                        $dueDateFormattedFinal = $dueDateFormatted->format('F j, Y g:i A');
+                                            $dueDate = $row['taskDueDate'];
+                                            $dueDateFormatted = new DateTime($dueDate);
+                                            $dueDateFormattedFinal = $dueDateFormatted->format('F j, Y g:i A');
 
-                                        echo '<tr>';
-                                            echo '<td style="width:20%;">' . $row['taskName'] . '</td>';
-                                            echo '<td style="width:20%;">' . $startDateFormattedFinal . '</td>';
-                                            echo '<td style="width:20%;">' . $dueDateFormattedFinal . '</td>';
-                                            if ($tasksStatusColorAssignment == "Completed" || $tasksStatusColorAssignment == "Completed") {
-                                                echo '<td style="width:20%;"><span class="account-status-badge green" style="margin-left:0;">' . $row['status'] . '</span></td>';
-                                            } else if ($tasksStatusColorAssignment == "Overdue" || $tasksStatusColorAssignment == "overdue") {
-                                               echo '<td style="width:20%;"><span class="account-status-badge red" style="margin-left:0;">' . $row['status'] . '</span></td>';
-                                            } else if ($tasksStatusColorAssignment == "Pending" || $tasksStatusColorAssignment == "pending") {
-                                               echo '<td style="width:20%;"><span class="account-status-badge yellow" style="margin-left:0;">' . $row['status'] . '</span></td>';
-                                            } else if ($tasksStatusColorAssignment == "Stuck" || $tasksStatusColorAssignment == "stuck") {
-                                               echo '<td style="width:20%;"><span class="account-status-badge red-dark" style="margin-left:0;">' . $row['status'] . '</span></td>';
-                                            }
-                                            echo '<td class="">
-                                                     <a href="/dashboard/administration/tasks/viewTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">View</a><a href="/dashboard/administration/tasks/deleteTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">Delete</a><a href="/dashboard/administration/tasks/editTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px;">Edit</a>
-                                                 </td>
-                                            ';
-                                        echo '</tr>';
+                                            echo '<tr>';
+                                                echo '<td style="width:20%;">' . $row['taskName'] . '</td>';
+                                                echo '<td style="width:20%;">' . $startDateFormattedFinal . '</td>';
+                                                echo '<td style="width:20%;">' . $dueDateFormattedFinal . '</td>';
+                                                if ($tasksStatusColorAssignment == "Completed" || $tasksStatusColorAssignment == "Completed") {
+                                                    echo '<td style="width:20%;"><span class="account-status-badge green" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Overdue" || $tasksStatusColorAssignment == "overdue") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Pending" || $tasksStatusColorAssignment == "pending") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge yellow" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Stuck" || $tasksStatusColorAssignment == "stuck") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red-dark" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                }
+                                                echo '<td class="">
+                                                        <a href="/dashboard/administration/tasks/viewTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">View</a><a href="/dashboard/administration/tasks/deleteTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">Delete</a><a href="/dashboard/administration/tasks/editTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px;">Edit</a>
+                                                    </td>
+                                                ';
+                                            echo '</tr>';
 
+                                        }
+
+                                        echo '</table>';
                                     }
+                                } if ($employeeAccessLevel == "Manager") {
+                                    $sql = "SELECT * FROM caliweb_tasks";
+                                    $result = mysqli_query($con, $sql);
 
-                                    echo '</table>';
+                                    if (mysqli_num_rows($result) > 0) {
+                                        // Output table header
+                                        echo '<table style="width:100%; margin-top:-3%;">
+                                                <tr>
+                                                    <th style="width:20%;">Task Name</th>
+                                                    <th style="width:20%;">Task Start Date</th>
+                                                    <th style="width:20%;">Task Due Date</th>
+                                                    <th style="width:20%;">Status</th>
+                                                    <th style="width:20%;">Actions</th>
+                                                </tr>';
+
+                                        // Output table rows
+                                        while ($row = mysqli_fetch_assoc($result)) {
+
+                                            $tasksStatusColorAssignment = $row['status'];
+
+                                            $startDate = $row['taskStartDate'];
+                                            $startDateFormatted = new DateTime($startDate);
+                                            $startDateFormattedFinal = $startDateFormatted->format('F j, Y g:i A');
+
+                                            $dueDate = $row['taskDueDate'];
+                                            $dueDateFormatted = new DateTime($dueDate);
+                                            $dueDateFormattedFinal = $dueDateFormatted->format('F j, Y g:i A');
+
+                                            echo '<tr>';
+                                                echo '<td style="width:20%;">' . $row['taskName'] . '</td>';
+                                                echo '<td style="width:20%;">' . $startDateFormattedFinal . '</td>';
+                                                echo '<td style="width:20%;">' . $dueDateFormattedFinal . '</td>';
+                                                if ($tasksStatusColorAssignment == "Completed" || $tasksStatusColorAssignment == "Completed") {
+                                                    echo '<td style="width:20%;"><span class="account-status-badge green" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Overdue" || $tasksStatusColorAssignment == "overdue") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Pending" || $tasksStatusColorAssignment == "pending") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge yellow" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Stuck" || $tasksStatusColorAssignment == "stuck") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red-dark" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                }
+                                                echo '<td class="">
+                                                        <a href="/dashboard/administration/tasks/viewTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">View</a><a href="/dashboard/administration/tasks/deleteTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">Delete</a><a href="/dashboard/administration/tasks/editTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px;">Edit</a>
+                                                    </td>
+                                                ';
+                                            echo '</tr>';
+
+                                        }
+
+                                        echo '</table>';
+                                    }
+                                } else {
+                                    $sql = "SELECT * FROM caliweb_tasks WHERE assignedUser = '$fullname'";
+                                    $result = mysqli_query($con, $sql);
+
+                                    if (mysqli_num_rows($result) > 0) {
+                                        // Output table header
+                                        echo '<table style="width:100%; margin-top:-3%;">
+                                                <tr>
+                                                    <th style="width:20%;">Task Name</th>
+                                                    <th style="width:20%;">Task Start Date</th>
+                                                    <th style="width:20%;">Task Due Date</th>
+                                                    <th style="width:20%;">Status</th>
+                                                    <th style="width:20%;">Actions</th>
+                                                </tr>';
+
+                                        // Output table rows
+                                        while ($row = mysqli_fetch_assoc($result)) {
+
+                                            $tasksStatusColorAssignment = $row['status'];
+
+                                            $startDate = $row['taskStartDate'];
+                                            $startDateFormatted = new DateTime($startDate);
+                                            $startDateFormattedFinal = $startDateFormatted->format('F j, Y g:i A');
+
+                                            $dueDate = $row['taskDueDate'];
+                                            $dueDateFormatted = new DateTime($dueDate);
+                                            $dueDateFormattedFinal = $dueDateFormatted->format('F j, Y g:i A');
+
+                                            echo '<tr>';
+                                                echo '<td style="width:20%;">' . $row['taskName'] . '</td>';
+                                                echo '<td style="width:20%;">' . $startDateFormattedFinal . '</td>';
+                                                echo '<td style="width:20%;">' . $dueDateFormattedFinal . '</td>';
+                                                if ($tasksStatusColorAssignment == "Completed" || $tasksStatusColorAssignment == "Completed") {
+                                                    echo '<td style="width:20%;"><span class="account-status-badge green" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Overdue" || $tasksStatusColorAssignment == "overdue") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Pending" || $tasksStatusColorAssignment == "pending") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge yellow" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                } else if ($tasksStatusColorAssignment == "Stuck" || $tasksStatusColorAssignment == "stuck") {
+                                                echo '<td style="width:20%;"><span class="account-status-badge red-dark" style="margin-left:0;">' . $row['status'] . '</span></td>';
+                                                }
+                                                echo '<td class="">
+                                                        <a href="/dashboard/administration/tasks/viewTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">View</a><a href="/dashboard/administration/tasks/deleteTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px; margin-right:10px;">Delete</a><a href="/dashboard/administration/tasks/editTask/task_id='.$row['id'].'" class="caliweb-button secondary no-margin margin-10px-right" style="padding:6px 24px;">Edit</a>
+                                                    </td>
+                                                ';
+                                            echo '</tr>';
+
+                                        }
+
+                                        echo '</table>';
+                                    }
                                 }
                             ?>
                         </div>
