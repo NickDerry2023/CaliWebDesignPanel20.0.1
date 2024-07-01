7ce04c1b66aaf9ebc0488bd75d0f25493a2b97ff
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-01 06:20:44 +0000

    Rearrage account view when the user selects an account to work with cause I am indecisive.

--- dashboard/customers/viewAccount/index.php
+++ dashboard/customers/viewAccount/index.php
@@ -27,10 +27,9 @@
 
             <section class="section caliweb-section customer-dashboard-greeting-section">
                 <div class="container caliweb-container">
-                    <p class="no-padding" style="font-size:16px;">Overview / Cali Web Design Standard</h4>
+                    <p class="no-padding" style="font-size:16px;">Overview / Cali Web Design View Account / Details</h4>
                 </div>
             </section>
-
 
             <section class="section caliweb-section customer-dashboard-section">
                 <div class="container caliweb-container">
@@ -38,92 +37,10 @@
                         <div class="accounts-overview">
                             <div class="caliweb-card dashboard-card" style="padding:0;">
                                 <div class="card-header no-padding no-margin customer-card-header" style="padding:20px;">
-                                    <?php
-                                        if (mysqli_num_rows($websiteresult) > 0)  {
-                                            mysqli_free_result($websiteresult);
-                                            $websiteDomainName = $websiteinfo['domainName'];
-                                            $websitePlan = $websiteinfo['servicePlan'];
-                                            $websiteRoot = $websiteinfo['websiteDIR'];
-                                            $websiteType = $websiteinfo['websiteType'];
-
-                                            $frontendlangType = $websiteinfo['fontendFramework'];
-                                            $backendlangType = $websiteinfo['backendLang'];
-                                            $databaseType = $websiteinfo['databaseSystem'];
-
-                                            $setupdate = $websiteinfo['setupDate'];
-                                            $setupdateformatted = new DateTime($setupdate);
-                                            $setupdateformattedfinal = $setupdateformatted->format('F j, Y g:i A');
-
-                                            echo '
-                                                <div id="full-card-area-container" class="display-flex width-100">
-                                                    <div id="image-container" class="thumb-web-img">
-                                                        <img src="https://image.thum.io/get/https://'.$websiteDomainName.'" alt="Website Preview" style="width: 100%; height: auto;">
-                                                    </div>
-                                                    <div class="display-flex width-100" style="justify-content:space-between !important;">
-                                                        <div id="text-container" style="width:55%;">
-                                                            <div class="caliweb-one-grid" style="grid-row-gap: 24px;">
-                                                                <div>
-                                                                    <p class="no-padding no-padding font-14px subtitle-text">DEPLOYMENT</p>
-                                                                    <p class="no-padding no-padding font-14px text-bold" style="padding-top:1%;">https://'.$websiteDomainName.'</p>
-                                                                </div>
-                                                                <div class="caliweb-three-grid" style="grid-template-columns: .3fr 1.2fr .5fr;">
-                                                                    <div>
-                                                                        <p class="no-padding no-padding font-14px subtitle-text">TYPE</p>
-                                                                        <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">'.$websiteType.'</p>
-                                                                    </div>
-                                                                    <div>
-                                                                        <p class="no-padding no-padding font-14px subtitle-text">PLAN</p>
-                                                                        <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">'.$websitePlan.'</p>
-                                                                    </div>
-                                                                    <div>
-                                                                        <p class="no-padding no-padding font-14px subtitle-text">HOME DIRECTORY</p>
-                                                                        <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">'.$websiteRoot.'</p>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="">
-                                                                    <p class="no-padding no-padding font-14px subtitle-text">SETUP DATE</p>
-                                                                    <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">'.$setupdateformattedfinal.'</p>
-                                                                </div>
-                                                                <div class="">
-                                                                    <p class="no-padding no-padding font-14px subtitle-text">LANGUAGE STACK</p>
-                                                                    <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">Front End is '.$frontendlangType.', Backend is '.$backendlangType.', Database is '.$databaseType.'</p>
-                                                                </div>
-                                                                <div class="">
-                                                                    <p class="no-padding no-padding font-14px subtitle-text">REPOSITORY</p>
-                                                                    <p class="no-padding no-padding font-14px text-bold" style="padding-top:5px;">--</p>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div id="buttons-container">
-                                                            <div class="display-flex align-center">
-                                                                <a href="" class="caliweb-button primary">Edit Account</a>
-                                                                <a href="" class="caliweb-button secondary">Close Account</a>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            ';
-                                        } else {
-                                            echo '
-                                                <h6 class="no-padding" style="font-size:16px; font-weight:600;">
-                                                    e
-                                                </h6>
-                                            ';
-                                        }
-                                    ?>
-                                </div>
-                                <div class="card-body">
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accounts-overview" style="margin-bottom:2%;">
-                            <div class="caliweb-card dashboard-card" style="padding:0;">
-                                <div class="card-header no-padding no-margin customer-card-header" style="padding:20px;">
-                                    <iframe id="shellinabox" style="border:0; outline:none; height:35vh;" class="width-100" src="https://<?php echo $_SERVER['HTTP_HOST'] ?>/terminal"></iframe>
-                                </div>
-                                <div class="card-body">
                                     
+                                </div>
+                                <div class="card-body">
+
                                 </div>
                             </div>
                         </div>
--- registration/index.php
+++ registration/index.php
@@ -127,7 +127,7 @@
     <!-- Unique Website Title Tag End -->
 
         <section class="login-container">
-            <div class="container caliweb-container" style="width:42%; margin-top:2%;">
+            <div class="container caliweb-container bigscreens-are-strange" style="width:42%; margin-top:2%;">
                 <div class="caliweb-login-box-header" style="text-align:left; margin-bottom:7%;">
                     <h3 class="caliweb-login-heading"><?php echo $orgshortname; ?> <span style="font-weight:700">Self Registration</span></h3>
                     <p style="font-size:12px; margin-top:-2%;">Welcome! We are excited to have you. Please fill out a few questions to setup your account with us.</p>
