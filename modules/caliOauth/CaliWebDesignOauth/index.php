84fd15c0df8d96107fdcc5a22e56e6bc49459338
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-04 07:47:00 +0000

    Restored Cali Oauth File to blank as im lay and dont want to code it.

--- modules/caliOauth/CaliWebDesignOauth/index.php
+++ modules/caliOauth/CaliWebDesignOauth/index.php
@@ -1,20 +1,3 @@
 <?php
 
-    $outsidesitecalled = False;
-    $oauthkey = "";
-    $siteid = "";
-
-    if ($siteid && $oauthkey == "" || $siteid && $oauthkey == NULL) {
-
-    } else {
-
-        // Loads the Cali Web Design Login Button when called.
-
-        if ($outsidesitecalled == "True") {
-            echo '<button class="" name="submit" id="caliwebdesignoauthbutton">Login with Cali Web Design</button>';
-        } else {
-            echo '';
-        }
-    }
-
 ?>
