c5cbfe11d9f1298cad9b66294081a1f3b01341ef
Nathan Schwartz <nathans@caliwebdesignservices.com>
2024-07-06 19:36:40 +0000

    Added a spanish language file. Translated some phrases into spanish.

---
+++ lang/es_es.php
@@ -0,0 +1,138 @@
+<?php
+// Cali Web Design Portal Version 20.0.8
+// Default Language file for Mexico Spanish
+
+// (C) Cali Web Design Services 2024 - It is illegal
+// to copy, reproduce, host, decompile or disassemble
+// this software without permission from Cali Web Design.
+
+// This is the Default Lang File and will be overwrtiten
+// when the panel updates. To prevent this and customize
+// this file please create a folder called overrides in
+// the lang directory and copy this file into that new
+// folder.
+
+// What you are about to do is edit all the possible word 
+// combinations for each page.
+
+// ====================== START LOGIN LANGUAGE AREA ==========================
+
+// Login Page Text Specifications
+$LANG_LOGIN_TITLE_PAR_1 = "Cali Web Design";
+$LANG_LOGIN_TITLE_PAR_2 = "Acceso";
+$LANG_LOGIN_AUTH_ERROR_TITLE = "Nombre de usuario o contraseña incorrecta";
+$LANG_LOGIN_AUTH_ERROR_TEXT = "El nombre de usuario o contraseña ingresado es incorrecto. Inténtelo de nuevo o contáctenos para restablecer su inicio de Acceso.";
+$LANG_LOGIN_BUTTON = "Continue";
+
+// Register Page Text Specifications
+$LANG_REGISTER_TITLE_PAR_1 = "Cali Web Design";
+$LANG_REGISTER_TITLE_PAR_2 = "Registration";
+$LANG_REGISTER_AUTH_ERROR_TITLE = "Cali Web Design Registration Error";
+$LANG_REGISTER_AUTH_ERROR_TEXT = "The registration proccess failed for an unknown reason. Please try again or if the problem presists contact Cali Web Design.";
+$LANG_REGISTER_CAPTCHA_ERROR_TITLE = "Cali Web Design reCaptcha Error";
+$LANG_REGISTER_CAPTCHA_ERROR_TEXT = "You did not check that silly little box. Please make sure youâ€™re not a robot. We require this to protect against spam.";
+$LANG_REGISTER_REGISTRATION_TITLE = "Already are a Cali Web Design customer?";
+$LANG_REGISTER_REGISTRATION_TEXT = "If you are a developer that needs access for testing please contact us for a sandbox account and API Credentials.";
+$LANG_REGISTER_BUTTON = "Continue";
+
+// ====================== START ERROR LANGUAGE AREA ==========================
+
+// License Error Text Specifications
+
+$LANG_LICENSE_TITLE_PAR_1 = "Cali Web Design";
+$LANG_LICENSE_TITLE_PAR_2 = "License Error";
+$LANG_LICENSE_TITLE = "The key you entered is not correct or malformed!";
+$LANG_LICENSE_TEXT = "To protect the Cali Web Design Panel Software you need a license to use it. The key you entered is not valid and can't be used. Contact Cali Web Design Services for a Genuine Key or make sure that the DB and Config File keys match.";
+
+// Registration Disabled Text Specifications
+
+$LANG_REG_DISABLED_TITLE_PAR_1 = "Cali Web Design";
+$LANG_REG_DISABLED_TITLE_PAR_2 = "Registration Disabled";
+$LANG_REG_DISABLED_TITLE = "This organization has disabled registrations!";
+$LANG_REG_DISABLED_TEXT = "We are currently not accepting new registrations. If you would like to become a customer please contact us.";
+
+// Generic Errors Text Specifications
+
+$LANG_GENERIC_ERROR_TITLE_PAR_1 = "Cali Web Design";
+$LANG_GENERIC_ERROR_TITLE_PAR_2 = "System Error";
+$LANG_GENERIC_ERROR_TITLE = "We encountered an unexpected error.";
+$LANG_GENERIC_ERROR_TEXT = "The action your performed has failed. We are working on a fix as soon as possible. We are sorry for any issues this may have caused. If you need assistance please contact Cali Web Design.";
+
+// Customer Security Quiz Text Specifications
+
+$LANG_CUSTOMER_VERIFICATION_TITLE_PAR_1 = "Cali Web Design";
+$LANG_CUSTOMER_VERIFICATION_TITLE_PAR_2 = "Security Quiz";
+$LANG_CUSTOMER_VERIFICATION_TITLE = "Cali Web Design needs to verify some information before proceeding.";
+$LANG_CUSTOMER_VERIFICATION_TEXT = "Please complete the short security quiz. The information in this security quiz is based off of public record.";
+$LANG_CUSTOMER_VERIFICATION_TEXT_PHONE_VERIFY = "Cali Web Design will send a 6 digit code to the phone number on file. Please enter it here.";
+
+
+// Account Suspension Error Text Specifications
+$LANG_SUSPENDED_TITLE = "We have suspended your account for suspicious activity.";
+$LANG_SUSPENDED_TEXT = "Your account at Cali Web Design has been suspended, please contact Cali Web Design Support during business hours in order to resolve this issue. Your services also have been suspended and can not be accessed.";
+$LANG_SUSPENDED_CONTACT_INFO = "To resolve this issue please call the number for your type of account. <br><br>For Cali Web Design Retail Customers call +1 (877)-597-7325. For Cali Web Design Business Customers call +1 (315)-898-1221. For Cali Web Design UK Customers call +44 (87) 0820 0077";
+
+// Account Terminated Error Text Specifications
+$LANG_TERMINATED_TITLE = "We have terminated your account.";
+$LANG_TERMINATED_TEXT = "Your account at Cali Web Design has been terminated in order to protect ourselves, this means you may no longer use Cali Web Design. This decision is final and can't be overturned.";
+
+// Account Under Review Text Specifications
+$LANG_ACCOUNT_UNDER_REVIEW_TITLE = 'Uh-Oh! We need more information to open your account.';
+$LANG_ACCOUNT_UNDER_REVIEW_TEXT = 'We need you to complete a few steps before you can start using our platform. Complete the <a href="/onboarding/" style="font-weight:700;" class="careers-link">form</a> to verify your account.';
+
+// Banned Error Text Specifications
+$LANG_BANNED_TITLE = "Hmmm. Your network seems a little unusual. We have banned your IP for security.";
+$LANG_BANNED_TEXT = "We cant verify the authenticity of this visit. We have banned this IP. Please disable any VPNs, Browser Extensions, or AD Blockers, and try your request again. This is an automated process.";
+
+// Payment Errors Text Specifications
+
+$LANG_PAYMENT_ERROR_TITLE_PAR_1 = "Cali Web Design";
+$LANG_PAYMENT_ERROR_TITLE_PAR_2 = "Payment Error";
+$LANG_PAYMENT_ERROR_TITLE = "We encountered an declined card error.";
+$LANG_PAYMENT_ERROR_TEXT = "The payment you attempted to make has failed. Please try again or use another card. We are sorry for any issues this may have caused. If you need assistance please contact support.";
+
+// ====================== START LOGOUT LANGUAGE AREA ==========================
+
+// Logout Text Specifications
+
+$LANG_LOGOUT_BASE_TEXT = "You have successfully signed out,";
+$LANG_LOGOUT_SECONDARY_TEXT = "You will be automatically be redirected in";
+$LANG_LOGOUT_REDIRECT_FALLBACK_TEXT = "If you are not redirected automatically you can";
+
+// ====================== START ONBOARDING LANGUAGE AREA ==========================
+
+// Onboarding Completed Text Specifications
+
+$LANG_ONBOARDCOMLETE_BASE_TEXT = "You have successfully completed onboarding,";
+$LANG_ONBOARDCOMLETE_SECONDARY_TEXT = "You will be automatically be redirected in";
+$LANG_ONBOARDCOMLETE_FALLBACK_TEXT = "If you are not redirected automatically you can";
+
+// ====================== START DASHBOARD LANGUAGE AREA ==========================
+
+// Dashboard Overview Text Specifications
+
+$LANG_DASH_HOME_TITLE = ""; // Leave Blank for no Double Greeting
+$LANG_CUSTOMER_HOME_ACCOUNTS_TITLE = "accounts";
+$LANG_QUICKACTIONS_TITLE = "Quick Actions";
+$LANG_EDIT_WEBSITES_TILE = "Edit Websites";
+$LANG_EDIT_WEBSITES_SUBTEXT = "Edit your website on the fly with an easy to use drag and drop interface.";
+$LANG_RUN_SPEEDTEST_TILE = "Run Speed Test";
+$LANG_RUN_SPEEDTEST_SUBTEXT = "Wondering how fast your site is? Run a speed test to find out.";
+$LANG_BACKUPS_TILE = "Backups";
+$LANG_BACKUPS_SUBTEXT = "Run scheduled backups and restore backups right within your portal.";
+$LANG_LOG_FILES_TILE = "Log Files";
+$LANG_LOG_FILES_SUBTEXT = "View your websites logs, account logs and what your users are doing.";
+$LANG_CODE_INTEGRITY_TILE = "Code Integrity";
+$LANG_CODE_INTEGRITY_SUBTEXT = "Code Integrity allows you to protect your website.";
+$LANG_MONITORING_TILE = "Monitoring";
+$LANG_MONITORING_SUBTEXT = "Have us monitor your website for any suspicious activity.";
+$LANG_PREAPPROVED_DASH_TITLE = "Plan for your next business";
+$LANG_NO_PREAPPROVED_DASH_TEXT = "There are currently no pre-approved offers.";
+$LANG_DOCUMENTATION_SECTION_TITLE = "Explore Documentation";
+
+// Dashboard Account Management Text Specifications
+
+$LANG_CUSTOMER_SERVICES_TITLE_TEXT = "Current Services";
+
+
+?>
