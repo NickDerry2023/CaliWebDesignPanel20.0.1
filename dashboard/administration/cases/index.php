ad21e4aa5d5e362c456acfc893a7519b5961ffe4
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-07 07:38:23 +0000

    [UPDATE] Created the code for the cases listing page as well as added image assets such as icons to that page.

--- dashboard/administration/cases/index.php
+++ dashboard/administration/cases/index.php
@@ -1,4 +1,47 @@
 <?php
+    $pagetitle = "Cases";
+    $pagesubtitle = "List";
 
+    include($_SERVER["DOCUMENT_ROOT"].'/assets/php/dashboardHeader.php');
+
+    if ($userrole == "Customer" || $userrole == "customer") {
+        header("location:/dashboard/customers");
+    } else if ($userrole == "Authorized User" || $userrole == "authorized user") {
+        header("location:/dashboard/customers/authorizedUserView");
+    } else if ($userrole == "Partner" || $userrole == "partner") {
+        header("location:/dashboard/partnerships");
+    }
+
+    echo '<title>'.$pagetitle.' - '.$pagesubtitle.'</title>';
+?>
+
+    <section class="section first-dashboard-area-cards">
+        <div class="container width-98">
+            <div class="caliweb-one-grid special-caliweb-spacing">
+                <div class="caliweb-card dashboard-card">
+                    <div class="card-header">
+                         <div class="display-flex align-center">
+                            <div class="no-padding margin-10px-right icon-size-formatted">
+                                <img src="/assets/img/systemIcons/cases.png" alt="Client Logo and/or Business Logo" style="background-color:#f5e6fe;" class="client-business-andor-profile-logo" />
+                            </div>
+                            <div>
+                                <p class="no-padding font-14px">Tasks</p>
+                                <h4 class="text-bold font-size-20 no-padding" style="padding-bottom:0px; padding-top:5px;">List Cases</h4>
+                            </div>
+                         </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="dashboard-table">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+
+    include($_SERVER["DOCUMENT_ROOT"].'/assets/php/dashboardFooter.php');
 
 ?>
