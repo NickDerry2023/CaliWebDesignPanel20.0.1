7d0564bd0924a7848a64040675542c895c1617fe
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-07 22:45:51 +0000

    [UPDATE] Cali Web Design Discord OAuth Module is now implemented, Just have to add support in other parts of the panel.

--- modules/caliOauth/discord/index.php
+++ modules/caliOauth/discord/index.php
@@ -1,4 +1,142 @@
 <?php
+    ob_start();
 
+    // Cali Web Design Discord OAuth Module
+    // Version: 2.0.1
+    // (C) Copyright Cali Web Design Corporation - All rights reserved
+    // DISMANTLING, REVERSE ENGINEERING, OR MODIFICATION OF THIS MODULE IS PROHIBITED.
 
+    // This module is not included in the Cali Panel software by default
+    // the Cali Web Design Discord OAuth Module will allow you to authenicate
+    // using your discord account into the Cali Panel. This can save time instead
+    // of needing to fill our registration form our you can just quickly
+    // authenicate.
+
+    // Signin Logic
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+    use Dotenv\Dotenv;
+
+    $discord_dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']."/modules/caliOauth/discord");
+    $discord_dotenv->load();
+
+    $discord_client_id = $_ENV['DISCORD_CLIENT_ID'];
+    $discord_client_secret = $_ENV['DISCORD_CLIENT_SECRET'];
+    $discord_redirect_uri = $_ENV['DISCORD_REDIRECT_URI'];
+
+    if ($_SESSION['pagetitle'] == "Account Settings") {
+
+        if (isset($_GET['code'])) {
+
+            $discord_newauth_redirect_uri = $_ENV['DISCORD_REDIRECT_NEWAUTH_URI'];
+
+            $discord_code = $_GET['code'];
+            $discord_token_request = array(
+                'client_id' => $discord_client_id,
+                'client_secret' => $discord_client_secret,
+                'grant_type' => 'authorization_code',
+                'code' => $discord_code,
+                'redirect_uri' => $discord_newauth_redirect_uri,
+                'scope' => 'identify email'
+            );
+        
+            $discord_curl = curl_init('https://discord.com/api/oauth2/token');
+            curl_setopt($discord_curl, CURLOPT_POST, true);
+            curl_setopt($discord_curl, CURLOPT_POSTFIELDS, http_build_query($discord_token_request));
+            curl_setopt($discord_curl, CURLOPT_RETURNTRANSFER, true);
+            $discord_token_response = curl_exec($discord_curl);
+            curl_close($discord_curl);
+            $discord_token_data = json_decode($discord_token_response, true);
+        
+            if (isset($discord_token_data['access_token'])) {
+                
+                // Fetch user information from Discord
+                $discord_access_token = $discord_token_data['access_token'];
+                $discord_curl = curl_init('https://discord.com/api/users/@me');
+                curl_setopt($discord_curl, CURLOPT_HTTPHEADER, array(
+                    'Authorization: Bearer ' . $discord_access_token
+                ));
+                curl_setopt($discord_curl, CURLOPT_RETURNTRANSFER, true);
+                $discord_user_response = curl_exec($discord_curl);
+                curl_close($discord_curl);
+                $discord_user_data = json_decode($discord_user_response, true);
+
+                $discord_id = $discord_user_data['id'];
+                
+                $update_query = "UPDATE `caliweb_users` SET `discord_id` = '$discord_id' WHERE `email` = '".$_SESSION['caliid']."'";
+                mysqli_query($con, $update_query);
+
+                echo '<script type="text/javascript">window.location = "/dashboard/"</script>';
+                    
+            } else {
+                echo '<script type="text/javascript">window.location = "/error/genericSystemError"</script>';
+            }
+        }
+
+    } else {
+
+        if (isset($_GET['code'])) {
+
+            $discord_code = $_GET['code'];
+            $discord_token_request = array(
+                'client_id' => $discord_client_id,
+                'client_secret' => $discord_client_secret,
+                'grant_type' => 'authorization_code',
+                'code' => $discord_code,
+                'redirect_uri' => $discord_redirect_uri,
+                'scope' => 'identify email'
+            );
+
+            $discord_curl = curl_init('https://discord.com/api/oauth2/token');
+            curl_setopt($discord_curl, CURLOPT_POST, true);
+            curl_setopt($discord_curl, CURLOPT_POSTFIELDS, http_build_query($discord_token_request));
+            curl_setopt($discord_curl, CURLOPT_RETURNTRANSFER, true);
+            $discord_token_response = curl_exec($discord_curl);
+            curl_close($discord_curl);
+            $discord_token_data = json_decode($discord_token_response, true);
+
+            if (isset($discord_token_data['access_token'])) {
+                
+                // Fetch user information from Discord
+                $discord_access_token = $discord_token_data['access_token'];
+                $discord_curl = curl_init('https://discord.com/api/users/@me');
+                curl_setopt($discord_curl, CURLOPT_HTTPHEADER, array(
+                    'Authorization: Bearer ' . $discord_access_token
+                ));
+                curl_setopt($discord_curl, CURLOPT_RETURNTRANSFER, true);
+                $discord_user_response = curl_exec($discord_curl);
+                curl_close($discord_curl);
+                $discord_user_data = json_decode($discord_user_response, true);
+
+                $discord_id = $discord_user_data['id'];
+                $discord_query = "SELECT * FROM `caliweb_users` WHERE `discord_id` = '$discord_id'";
+                $discord_result = mysqli_query($con, $discord_query);
+                $discord_rows = mysqli_num_rows($discord_result);
+                if ($discord_rows == 1) {
+
+                    $_SESSION['calidiscordid'] = $discord_id;
+                    echo '<script type="text/javascript">window.location = "/dashboard"</script>';
+                } else {
+
+                    echo '<script type="text/javascript">window.location = "/login"</script>';
+                }
+            } else {
+
+                echo '<script type="text/javascript">window.location = "/login"</script>';
+            }
+        }
+
+        if (isset($_SESSION['calidiscordid'])) {
+
+            // Perform query
+            $discord_query = mysqli_query($con, "SELECT * FROM caliweb_users WHERE discord_id = '".$_SESSION['calidiscordid']."'");
+            $discord_info = mysqli_fetch_array($discord_query);
+            // Free result set
+            mysqli_free_result($discord_query);
+
+            $caliid = $discord_info['email'];
+            $_SESSION['caliid'] = $caliid;
+        }
+    }
 ?>
