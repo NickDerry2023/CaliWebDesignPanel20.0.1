27544daf584c5d4b25a7483f225eb6fc175be4d0
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-10 04:40:48 +0000

    [UPDATE] Changed DB calls and file spacing in the Calendar Module.

--- modules/caliCalendar/fetchEvents.php
+++ modules/caliCalendar/fetchEvents.php
@@ -1,21 +1,26 @@
 <?php
-require_once 'dbConfig.php';
 
-$where_sql = '';
-if(!empty($_GET['start']) && !empty($_GET['end'])){
-    $where_sql .= " WHERE start BETWEEN '".$_GET['start']."' AND '".$_GET['end']."' ";
-}
+    require($_SERVER["DOCUMENT_ROOT"].'/configuration/index.php');
 
-$sql = "SELECT * FROM events $where_sql";
-$result = $db->query($sql);
+    $where_sql = '';
+    if(!empty($_GET['start']) && !empty($_GET['end'])){
 
-$eventsArr = array();
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        array_push($eventsArr, $row);
+        $where_sql .= " WHERE start BETWEEN '".$_GET['start']."' AND '".$_GET['end']."' ";
+
     }
-}
 
-echo json_encode($eventsArr);
+    $sql = "SELECT * FROM events $where_sql";
+    $result = $con->query($sql);
+
+    $eventsArr = array();
+    if($result->num_rows > 0){
+
+        while($row = $result->fetch_assoc()){
+            array_push($eventsArr, $row);
+        }
+
+    }
+
+    echo json_encode($eventsArr);
 
 ?>
