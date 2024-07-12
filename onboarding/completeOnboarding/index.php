ebe4d9c52ab0f9a4ebb2f781663c2d4484842234
Nick Derry <nickd@caliwebdesignservices.com>
2024-07-12 06:57:23 +0000

    [UPDATE] Created the other decision pages for onboarding like risk review, calling online team, or branch requirement pages.

---
+++ onboarding/completeOnboarding/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    // This page should not render any HTML content other than a preloader, this
+    // script will be used to determine account approval. If the account needs
+    // more verification throw an error to do so.
+
+    
+
+?>
---
+++ onboarding/decision/emailRiskTeam/index.php
@@ -0,0 +1,74 @@
+<?php
+    include($_SERVER["DOCUMENT_ROOT"]."/lang/en_US.php");
+    require($_SERVER["DOCUMENT_ROOT"].'/configuration/index.php');
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+    use Dotenv\Dotenv;
+
+    // Load environment variables from .env file
+    $dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+    $dotenv->load();
+
+    // Get database credentials from environment variables
+    $licenseKeyfromConfig = $_ENV['LICENCE_KEY'];
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+
+    // Perform query
+    $result = mysqli_query($con, "SELECT * FROM caliweb_panelconfig WHERE id = '1'");
+    $panelinfo = mysqli_fetch_array($result);
+    // Free result set
+    mysqli_free_result($result);
+
+    $panelname = $panelinfo['panelName'];
+    $paneldomain = $panelinfo['panelDomain'];
+    $orgshortname = $panelinfo['organizationShortName'];
+    $orglogolight = $panelinfo['organizationLogoLight'];
+    $orglogodark = $panelinfo['organizationLogoDark'];
+    $licenseKeyfromDB = $panelinfo['panelKey'];
+
+
+    if ($licenseKeyfromConfig == $licenseKeyfromDB) {
+
+    include($_SERVER["DOCUMENT_ROOT"]."/assets/php/loginHeader.php");
+
+    echo '<title>Your account was approved!</title>';
+
+?>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+
+    <section class="section purchase-complete" style="padding-top:0%; padding-left:10%;">
+        <div class="container caliweb-container">
+            <div style="display:flex; align-items:center;">
+                <div>
+                    <img src="<?php echo $orglogolight; ?>" class="caliweb-navbar-logo-img light-mode" style="width:15%; margin-top:12%;" />
+                    <img src="<?php echo $orglogodark; ?>" class="caliweb-navbar-logo-img dark-mode" style="width:15%; margin-top:12%;" />
+                    <h6 style="font-weight:700; margin:0; padding:0; margin-top:5%; margin-bottom:5%;">This application seems potentially fradulent</h6>
+                    <p class="caliweb-login-sublink license-text-dark width-100">Please email our risk review department at support@caliwebdesignservices.com to resolve this, they typically respond within 24 hours.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="caliweb-login-footer license-footer">
+        <div class="container caliweb-container">
+            <div class="caliweb-grid-2">
+                <div class="">
+                    <p class="caliweb-login-footer-text">&copy; 2024 - Cali Web Design Corporation - All rights reserved. It is illegal to copy this website.</p>
+                </div>
+                <div class="list-links-footer">
+                    <a href="'.$paneldomain.'/terms">Terms of Service</a>
+                    <a href="'.$paneldomain.'/privacy">Privacy Policy</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+    include($_SERVER["DOCUMENT_ROOT"].'/assets/php/loginFooter.php');
+
+    } else {
+        header("Location: /error/licenseInvalid");
+    }
+?>
