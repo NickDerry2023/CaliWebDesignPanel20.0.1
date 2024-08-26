acbcfbd2a8a463bdc307fe8a90f403dbdfc709a5
Nick Derry <nickd@caliwebdesignservices.com>
2024-08-26 07:22:14 +0000

    [UPDATE] Started preliminary support for Campaings, Leads, and Contacts. The file structure is now present as well as the inital pages go load actual content.

--- dashboard/administration/contacts/index.php
+++ dashboard/administration/contacts/index.php
@@ -5,14 +5,40 @@
     $pagetype = "Administration";
 
     include($_SERVER["DOCUMENT_ROOT"].'/modules/CaliWebDesign/Utility/Backend/Dashboard/Headers/index.php');
-
-    echo '<title>'.$pagetitle.' | '.$pagesubtitle.'</title>';
     
+    echo '<title>'.$pagetitle.' | '.$pagesubtitle.'</title>';
+
 ?>
 
     <section class="section first-dashboard-area-cards">
         <div class="container width-98">
             <div class="caliweb-one-grid special-caliweb-spacing">
+
+                <div class="caliweb-card dashboard-card">
+                    <div class="card-header">
+                        <div class="display-flex align-center" style="justify-content: space-between;">
+                            <div class="display-flex align-center">
+                                <div class="no-padding margin-10px-right icon-size-formatted">
+                                    <img src="/assets/img/systemIcons/contactsicon.png" alt="Contacts Page Icon" style="background-color:#f5e6fe;" class="client-business-andor-profile-logo" />
+                                </div>
+                                <div>
+                                    <p class="no-padding font-14px">Contacts</p>
+                                    <h4 class="text-bold font-size-20 no-padding" style="padding-bottom:0px; padding-top:5px;">List Contacts</h4>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="/dashboard/administration/contacts/createContact/" class="caliweb-button primary no-margin margin-10px-right" style="padding:6px 24px;">Create New</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="dashboard-table">
+                            
+                            NULL
+
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </div>
--- dashboard/administration/leads/index.php
+++ dashboard/administration/leads/index.php
@@ -1,16 +1,44 @@
 <?php
+
     $pagetitle = "Leads";
     $pagesubtitle = "List of Leads";
     $pagetype = "Administration";
 
     include($_SERVER["DOCUMENT_ROOT"].'/modules/CaliWebDesign/Utility/Backend/Dashboard/Headers/index.php');
-
+    
     echo '<title>'.$pagetitle.' | '.$pagesubtitle.'</title>';
+
 ?>
 
     <section class="section first-dashboard-area-cards">
         <div class="container width-98">
             <div class="caliweb-one-grid special-caliweb-spacing">
+
+                <div class="caliweb-card dashboard-card">
+                    <div class="card-header">
+                        <div class="display-flex align-center" style="justify-content: space-between;">
+                            <div class="display-flex align-center">
+                                <div class="no-padding margin-10px-right icon-size-formatted">
+                                    <img src="/assets/img/systemIcons/leadsicon.png" alt="Leads Page Icon" style="background-color:#f5e6fe;" class="client-business-andor-profile-logo" />
+                                </div>
+                                <div>
+                                    <p class="no-padding font-14px">Leads</p>
+                                    <h4 class="text-bold font-size-20 no-padding" style="padding-bottom:0px; padding-top:5px;">List Leads</h4>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="/dashboard/administration/leads/createLead/" class="caliweb-button primary no-margin margin-10px-right" style="padding:6px 24px;">Create New</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="dashboard-table">
+                            
+                            NULL
+
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </div>
