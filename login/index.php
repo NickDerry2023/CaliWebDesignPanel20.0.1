e98f58f801b5d63c3ccd3ba8f86028e09374cd40
Nick Derry <nickd@caliwebdesignservices.com>
2024-08-29 05:54:02 +0000

    [UPDATE] Login will now check and ban IPs if the amount of unsuccessful login attempts is more than 5.

--- login/index.php
+++ login/index.php
@@ -14,25 +14,275 @@
 
     }
 
+    // IP Address Checking and Banning
+
+    function getClientIp() {
+
+        $keys = [
+            'HTTP_CLIENT_IP', 
+            'HTTP_X_FORWARDED_FOR', 
+            'HTTP_X_FORWARDED', 
+            'HTTP_FORWARDED_FOR', 
+            'HTTP_FORWARDED', 
+            'REMOTE_ADDR'
+        ];
+
+        foreach ($keys as $key) {
+
+            if ($ipaddress = getenv($key)) {
+
+                return $ipaddress;
+
+            }
+
+        }
+
+        return 'UNKNOWN';
+    }
+
+    $clientIp = getClientIp();
+
+    function isIpBlocked($ip, $con) {
+
+        $query = "SELECT COUNT(*) FROM caliweb_networks WHERE ipAddress = ? AND listType = 'blacklist'";
+
+        if ($stmt = $con->prepare($query)) {
+
+            $stmt->bind_param('s', $ip);
+
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+
+            $count = $result->fetch_array()[0];
+            
+            $stmt->close();
+
+            return $count > 0;
+
+        }
+
+        return false;
+
+    }
+
+    function isIpAllowed($ip, $con) {
+
+        $query = "SELECT COUNT(*) FROM caliweb_networks WHERE ipAddress = ? AND listType = 'whitelist'";
+
+        if ($stmt = $con->prepare($query)) {
+
+            $stmt->bind_param('s', $ip);
+
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+
+            $count = $result->fetch_array()[0];
+
+            $stmt->close();
+
+            return $count > 0;
+
+        }
+
+        return false;
+
+    }
+
+    function isIpBlacklistedOrProxyVpn($ip, $passableUserId, $passableApiKey) {
+
+        $url = "https://neutrinoapi.net/ip-probe";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(['ip' => $ip]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["User-ID: $passableUserId", "API-Key: $passableApiKey"]);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($response, true);
+
+        if (isset($data['is-hosting']) && $data['is-hosting']) {
+
+            return true;
+
+        }
+
+        if (isset($data['is-proxy']) && $data['is-proxy']) {
+
+            return true;
+
+        }
+
+        if (isset($data['is-vpn']) && $data['is-vpn']) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    function hasAdBlocker() {
+
+        if (!isset($_SESSION['ad_blocker_checked'])) {
+            echo "<script>
+                var adBlockEnabled = false;
+                var testAd = document.createElement('div');
+                testAd.innerHTML = '&nbsp;';
+                testAd.className = 'adsbox';
+                document.body.appendChild(testAd);
+                window.setTimeout(function() {
+                    if (testAd.offsetHeight === 0) {
+                        adBlockEnabled = true;
+                    }
+                    testAd.remove();
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', 'check_ad_blocker.php', true);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    xhr.send('adBlockEnabled=' + adBlockEnabled);
+                }, 100);
+            </script>";
+
+            $_SESSION['ad_blocker_checked'] = true;
+
+        }
+
+        if (isset($_SESSION['adBlockEnabled']) && $_SESSION['adBlockEnabled']) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['adBlockEnabled'])) {
+
+        $_SESSION['adBlockEnabled'] = $_POST['adBlockEnabled'] == 'true' ? true : false;
+
+        exit;
+
+    }
+
+    function isIPSpamListed($ip, $passableUserId, $passableApiKey) {
+
+        $url = "https://neutrinoapi.net/host-reputation";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            'host' => $ip,
+            'list-rating' => '3',
+            'zones' => ''
+        ]));
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["User-ID: $passableUserId", "API-Key: $passableApiKey"]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($response, true);
+
+        if (isset($data['is-listed']) && $data['is-listed']) {
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+    function banIp($ip) {
+
+        header("Location: /error/bannedUser");
+
+        exit;
+
+    }
+
+    // Assuming $pdo is your PDO connection
+
+    if (!isIpAllowed($clientIp, $con)) {
+
+        if (isIpBlacklistedOrProxyVpn($clientIp, $passableUserId, $passableApiKey)) {
+
+            banIp($clientIp);
+
+        }
+
+        if (isIPSpamListed($clientIp, $passableUserId, $passableApiKey)) {
+
+            banIp($clientIp);
+
+        }
+
+        if (hasAdBlocker()) {
+
+            banIp($clientIp);
+
+        }
+
+        if (isIpBlocked($clientIp, $con)) {
+
+            banIp($clientIp);
+
+        }
+
+    }
+
     if (isset($_POST['emailaddress'])) {
 
         try {
 
             $cali_id = stripslashes($_REQUEST['emailaddress']);
+
             $cali_id = mysqli_real_escape_string($con, $cali_id);
+
             $password = stripslashes($_REQUEST['password']);
+
             $password = mysqli_real_escape_string($con, $password);
+
+            $client_ip = $_SERVER['REMOTE_ADDR'];
             
             $query = "SELECT * FROM `caliweb_users` WHERE `email` = '$cali_id' AND `password` = '". hash("sha512", $password)."'";
+
             $result = mysqli_query($con, $query);
 
             if (mysqli_num_rows($result) == 1) {
                 
+                unset($_SESSION['failed_attempts']);
+
                 $_SESSION['caliid'] = $cali_id;
+
                 header("Location: /dashboard");
+
                 exit;
 
             } else {
+
+                if (!isset($_SESSION['failed_attempts'])) {
+
+                    $_SESSION['failed_attempts'] = 0;
+
+                }
+
+                $_SESSION['failed_attempts']++;
+
+                if ($_SESSION['failed_attempts'] > 5) {
+
+                    $ban_query = "INSERT INTO `caliweb_networks` (`ipAddress`, `listType`) VALUES ('$client_ip', 'Blacklist')";
+
+                    mysqli_query($con, $ban_query);
+
+                    header("location: /error/bannedUser");
+                }
+
 
                 $login_error = true;
 
