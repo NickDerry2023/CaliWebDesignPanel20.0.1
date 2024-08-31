268671e64139df2a82a162c3a95a0a91302c20f5
Nick Derry <nickd@caliwebdesignservices.com>
2024-08-31 08:43:05 +0000

    [IMPROVEMENT] Automation script is now added, there is no logic, this script will query all services using a chron job every 3 minutes to see if the services are ready for customer use.

---
+++ automations/serviceDetection/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    // This script will run to check to see if services are down.
+    // It will then report the outages to staff by email and text message.
+    // after that it will update the panel.
+
+    // Service Checker Version 1.0.1
+
+?>
